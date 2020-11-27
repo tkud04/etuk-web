150e2d9d9e1396b2a6093680dad365c71366c520
Tobi K <32499735+tkud04@users.noreply.github.com>
2020-11-27 12:48:55 +0100

    Update contact.blade.php

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -23,14 +23,13 @@ $contacts = [
 ?>
 
 <center><img src="http://etukng.tobi-demos.tk/img/etukng.png" width="150" height="100"/></center>
-<h3 style="background: #be831d; color: #fff; padding: 10px 15px;">New message for {{$dept}}</h3>
-Hello admin,<br> you have a new message from <em>{{$name}}</em>:<br><br>
-Customer: <b>{{$em}}</b><br>
-Department: <b>{{$dept}}</b><br>
-Subject: <b>{{$subject}}</b><br>
-Message: <blockquote>{{$msg}}</blockquote><br>
+<h3 style="background: #be831d; color: #fff; padding: 10px 15px;">{{$subject}}</h3>
+Hello admin,<br><br> you have a new message from <em>{{$name}}</em>:<br><br>
+Customer: <a href="mailto:{{$em}}">{{$em}}</a><br><br>
+Dept: <b>{{$dept}}</b><br><br>
+Message: <blockquote><em>{{$msg}}</em></blockquote><br>
 
 <h5 style="background: #be831d; color: #fff; padding: 10px 15px;">Next steps</h5>
 
-<p>Kindly forward the message to the appropriate department and get bak to the customer</p><br>
+<p>Kindly forward the message to the appropriate department and get back to the customer</p><br>
 <p style="color:red;"><b>NOTE:</b> This is a test email and not the final version.</p><br><br>
