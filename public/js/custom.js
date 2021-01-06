77d7efd11de4d211b8e00f7e97ebee52bb9e4b84
Tkud <kudayisitobi@gmail.com>
2021-01-06 15:22:56 +0100

    rounding up

--- public/js/custom.js
+++ public/js/custom.js
@@ -441,26 +441,32 @@ $(function() {
 	
 	// Featured Slick Slider
 	$('.featured-slick-slide').slick({
-		centerMode: true,
-		centerPadding: '80px',
-		slidesToShow:1,
+		//centerMode: true,
+		//centerPadding: '80px',
+		//slidesToShow:1,
+		autoplay: true,
+        autoplaySpeed: 2000,
 		responsive: [
 		{
 		breakpoint: 768,
 		settings: {
 		arrows:true,
-		centerMode: true,
-		centerPadding: '60px',
-		slidesToShow:1
+		//centerMode: true,
+		//centerPadding: '60px',
+		//slidesToShow:1,
+		autoplay: true,
+        autoplaySpeed: 2000,
 		}
 		},
 		{
 		breakpoint: 480,
 		settings: {
 		arrows: false,
-		centerMode: true,
-		centerPadding: '40px',
-		slidesToShow: 1
+		//centerMode: true,
+		//centerPadding: '40px',
+		//slidesToShow: 1,
+		autoplay: true,
+        autoplaySpeed: 2000,
 		}
 		}
 		]
