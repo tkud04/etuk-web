912fbd784fae5bb3f9f33d2974ebf183f69e4ba7
Tkud <kudayisitobi@gmail.com>
2021-01-13 17:33:01 +0100

    rounding up split payments

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2461,6 +2461,30 @@ class MainController extends Controller {
 							 'percentage_charge' => "20"
 					    ];
                          $sa = $this->helpers->createSubAccount($data);
+						 
+						 //create spli group
+						 /**
+						 curl https://api.paystack.co/split
+-H "Authorization: Bearer YOUR_SECRET_KEY"
+-H "Content-Type: application/json"
+-d '{ "name":"Percentage Split", 
+      "type":"percentage", 
+      "currency": "NGN",
+      "subaccounts":[{
+        "subaccount": "ACCT_z3x6z3nbo14xsil",
+        "share": 20
+    },
+    {
+        "subaccount": "ACCT_pwwualwty4nhq9d",
+        "share": 30
+    }], 
+      "bearer_type":"subaccount", 
+      "bearer_subaccount":"ACCT_hdl8abxl8drhrl3"
+    }'
+-X POST
+						 **/
+						 
+						 
 					   }
 						 $ret = ['status' => "ok"];
 					}
