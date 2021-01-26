4ac5e07e953ff917fa0c1d8cbc7e32f065e98045
Tkud <kudayisitobi@gmail.com>
2021-01-26 17:12:42 +0100

    changed paystack acct

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -141,8 +141,8 @@ $subtitle = "List of bookings made by you";
 												<a href="{{$ru}}" target="_blank" class="button gray approve"><i class="ti-printer"></i> Receipt</a>
 												@if($is != "completed")
 												@if($o['status'] == "paid")
-												<a href="javascript:void(0)" onclick="checkoutApartment({xf: '{{$i['id']}}'})" class="button gray reject"><i class="ti-close"></i> Checkout</a>
 												<a data-toggle="modal" data-target="#booking-send-message" onclick="addXF({xf: '{{$o['id']}}',a: '{{$apartment['name']}}',type:'booking-send-message',gh:'g'})" class="rate-review"><i class="ti-email"></i> Send Message</a>
+												<a href="javascript:void(0)" onclick="checkoutApartment({xf: '{{$i['id']}}'})" class="button gray reject"><i class="ti-close"></i> Checkout</a>
 												@elseif($o['status'] == "unpaid")
 												<a data-toggle="modal" data-target="#booking-pay-now" onclick="addXF({xf: '{{$o['id']}}',type:'booking-pay-now'})" class="button gray reject"><i class="ti-card"></i> Pay now</a>
 											   <a href="javascript:void(0)" onclick="cancelBooking({xf: '{{$i['id']}}'})" class="button gray reject"><i class="ti-trash"></i> Cancel</a>
